a8d407b87e1756e7444b5b6708630a04444a56ed
f1uk3r <agarwal.umang4321@gmail.com>
2020-05-12 21:42:04 +0530

    added when number of events are given

--- proportion_ci.py
+++ proportion_ci.py
@@ -1,5 +1,6 @@
 import math
 import webbrowser
+import scipy.stats as st
 def stdError1(p, N, n):
 	return round(math.sqrt((p * (1 - p)) / n) * math.sqrt((N-n)/(N-1)), 4)
 def stdError2(p, n):
@@ -8,24 +9,28 @@ def error(z, sd):
 	return round((z * sd), 4)
 def stdErrorTwoProportion(p1,p2,n1,n2):
 	return round(math.sqrt(((p1*(1-p1))/n1)+((p2*(1-p2))/n2)),4)
-zs = dict([(70, 1.04), (75, 1.15), (80, 1.282), (85, 1.44), (90, 1.645), (92, 1.75), (95, 1.96), (96, 2.05), (98, 2.33), (99, 2.576), (99.5, 2.807), (99.9, 3.291)])
 option = int(input("Choose 1 for Confidence interval, 2 for margin of error and 3 for both: "))
 typ = int(input("Choose 1 if you have to find confidence interval on the difference between two proportion: "))
 pop = int(input("Population size given? 1. for yes"))
+proportionOrNumber = int(input("Give 1. proportion or 2. number of event"))
 if option == 1 and typ != 1:
-	p = float(input("Sample proportion (\\hat{p}) = "))
+	if proportionOrNumber == 1:
+		p = float(input("\\hat p = "))
+		n = int(input("Total number of sample (n) = "))
+	elif proportionOrNumber == 2:
+		n = int(input("Total number of sample (n) = "))
+		x = int(input("number of favourable events (X) = "))
+		p = x/n
+		print(f"\\hat p = X/n = {x}/{n} = {p}")
 	if pop == 1:
 		N = int(input("Population size (N) = "))
-	n = int(input("Sample size (n) = "))
 	if pop == 1:
 		sd = stdError1(p, N, n)
 	else:
 		sd = stdError2(p, n)
 	ci = int(input("Confidence interval(in %) = "))
 	
-	z = zs.get(ci, 0)
-	if z == 0:
-		z = float(input("z = "))
+	z = round(st.norm.ppf(1-((1-(ci/100))/2)), 4)
 	print ("z @ " + str(ci) + "% = " + str(z))
 	E = error(z, sd)
 	print ("Since we know that")
